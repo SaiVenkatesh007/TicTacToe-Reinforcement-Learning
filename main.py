bd3a91184ae7f5b492121b65bb90f35afaff0a2b
SaiVenkatesh007 <saivenki65@gmail.com>
2024-02-29 17:10:05 +0530

    Update
    Added Input Ability

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from agent import Agent
 from human import Human
 from game import TicTacToe
 
-p1 = Agent("Computer", epsilon=0)
-p1.loadPolicy("policy_p1")
+finishLoop = False
 
-p2 = Human("Human")
+bot = Agent("Computer", epsilon=0)
+bot.loadPolicy("policy_p1")
 
-game = TicTacToe(p1, p2)
-game.playWithHuman()
+playerName = input("Enter your name: ")
+
+while not finishLoop:
+    human = Human(playerName)
+
+    game = TicTacToe(bot, human)
+    game.playWithHuman()
+
+    print("If wish to play another game. Press\n    - 0 to Continue\n    - 1 to Quit")
+    index = int(input("Enter your Choice: "))
+
+    finishLoop = index==1
